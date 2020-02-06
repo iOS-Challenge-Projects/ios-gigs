42dd122f7399a205a34eae6c92fde00c068cd459
fritzgt <tuanis@live.com>
2020-02-06 08:35:38 +0200

    Created model files

---
+++ Gigs/Gigs/Model/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by FGT MAC on 2/6/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ Gigs/Gigs/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by FGT MAC on 2/6/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
