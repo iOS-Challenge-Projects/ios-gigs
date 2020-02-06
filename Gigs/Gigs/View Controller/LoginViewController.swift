33c96341959313fb2566f2f895c7ca242aca5cd2
fritzgt <tuanis@live.com>
2020-02-06 08:29:54 +0200

    initial commit

---
+++ Gigs/Gigs/View Controller/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by FGT MAC on 2/5/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK: - Actions
+    @IBAction func actionSegmentedButton(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func actionButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
